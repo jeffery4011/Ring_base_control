a7b361322b707df11cff2fc0bedf879fadaa0996
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-01-25 12:20:55 -0500

    update

--- locomote.py
+++ locomote.py
@@ -4,6 +4,10 @@ from running_std import IMU_reset
 
 if __name__ == '__main__':
     while True:
-        if read_IMU():
-            One_cycle_Shrink_Expand()
-            IMU_reset()
+        try:
+            if read_IMU():
+                One_cycle_Shrink_Expand()
+                IMU_reset()
+        except KeyboardInterrupt:
+            print('Stop!')
+            break

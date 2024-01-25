92efdb43b7650a3523a27b286705628724e3f77f
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-01-25 14:22:35 -0500

    Update locomote.py

--- locomote.py
+++ locomote.py
@@ -2,13 +2,17 @@ from Shrink_and_expand import One_cycle_Shrink_Expand
 from running_std import read_IMU
 from running_std import IMU_reset
 from Shrink_and_expand import All_stop
+import time
 
 if __name__ == '__main__':
+    diffusion_limit = 10
+    pt_time = time.time()
     while True:
         try:
-            if read_IMU():
+            if read_IMU() and (time.time()-pt_time>diffusion_limit):
                 One_cycle_Shrink_Expand()
                 IMU_reset()
+                pt_time = time.time()
         except KeyboardInterrupt:
             print('Stop!')
             All_stop()

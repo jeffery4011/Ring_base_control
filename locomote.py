a45195374f4e5fbc6e38c4acdce7e9124007ca79
jeffery4011 <jeffery_4011@qq.com>
2024-01-25 12:13:52 -0500

    modified

--- locomote.py
+++ locomote.py
@@ -1,6 +1,6 @@
 from Shrink_and_expand import One_cycle_Shrink_Expand
 from running_std import read_IMU
-from running_std improt IMU_reset
+from running_std import IMU_reset
 
 if __name__ == '__main__':
     while True:

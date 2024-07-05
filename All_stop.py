683fd7539a6890dddcaa336e1d64d7d4fd9022b3
jeffery4011 <41932265+jeffery4011@users.noreply.github.com>
2024-07-05 17:19:12 -0400

    Create All_stop.py

---
+++ All_stop.py
@@ -0,0 +1,15 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import numpy as np
+import cv2
+import os
+import math
+from Shrink_and_expand import shrink_procedure
+from Shrink_and_expand import All_stop
+
+if __name__ == "__main__":
+    # shrink_procedure()
+    # expand_procedure()
+    # dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, 1, ADDR_GOAL_VELOCITY, 0)
+    All_stop()
